7cd15fb6453b4173d96c121ca24945bdd34d0eb3
Dhairya <132248847+Dhairya432r@users.noreply.github.com>
2023-12-11 18:27:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb+srv://Dhairya:Dhairya432r@cluster0.kad7z2w.mongodb.net
 
 const app = express();
 const corsOptions = {
-  origin: 'https://Dhairya432r.github.io/Dhairya432rp/',
+  origin: 'https://dhairya432r.github.io',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
