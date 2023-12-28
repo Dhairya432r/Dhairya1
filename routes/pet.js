531c68aef0cf1285b8be713cf29d1e1daeaa7bbb
Dhairya <210346620240@paruluniversity.ac.in>
2023-12-28 18:33:22 +0530

    Dhairya1

--- routes/pet.js
+++ routes/pet.js
@@ -5,7 +5,7 @@ const Pet = require('../models/User');
 const cors = require('cors')
 //hello
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://dhairya432r.github.io',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
