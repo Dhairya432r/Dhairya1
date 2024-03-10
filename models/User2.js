15b213ab95167df94b669c02422bb54d513be3fc
Dhairya <132248847+Dhairya432r@users.noreply.github.com>
2024-03-10 20:53:55 +0530

    Create User2.js

---
+++ models/User2.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const grommingSchema = new mongoose.Schema({
+    type:{
+        type:String,
+        required: true
+    },
+    name: {
+        type:String,
+        required: true
+    },
+    gender:{
+        type:String,
+        required: true
+    },
+    size:{
+        type:String,
+        required: true
+    },
+    old:{
+        type:String,
+        required: true
+    },
+    aggressive:{
+        type:String,
+        required: true
+    },
+    date:{
+        type:String,
+        required: true
+    },
+    time:{
+        type:String,
+        requiredd: true
+    }
+
+});
+const Gromming = mongoose.model("Gromming",grommingSchema)
+module.exports = Gromming;
