8a1b0f9e37d5c5a54fac53b8927f23155eeea153
Dhairya <132248847+Dhairya432r@users.noreply.github.com>
2024-03-10 21:03:46 +0530

    Update pet.js

--- routes/pet.js
+++ routes/pet.js
@@ -5,7 +5,7 @@ const Pet = require('../models/User');
 const cors = require('cors')
 //hello
 const corsOptions = {
-  origin: 'https://dhairya432r.github.io',
+  origin: 'https://dhairya432r.github.io/Dhairya432rp/',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
